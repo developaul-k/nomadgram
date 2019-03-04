e8e6d31a5a7ffd356cec0762d0c71b82197ab151
김영주 <pual@gim-yeongjuui-MacBook-Pro.local>
2019-03-04 23:01:38 +0900

    Unignored build

---
+++ frontend/build/precache-manifest.9df8c13662100a951a1f765e2d38c212.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d17c0469c60375a01091fcee28f3749",
+    "url": "/static/media/phone.5d17c046.png"
+  },
+  {
+    "revision": "806d14d32a6ea4dda52960f04df5c449",
+    "url": "/static/media/logo.806d14d3.png"
+  },
+  {
+    "revision": "b610d46d4287334782e3db7a223fd5fe",
+    "url": "/static/media/loading.b610d46d.png"
+  },
+  {
+    "revision": "f06b908907d5d4f2aaf733e2bee7ea8e",
+    "url": "/static/media/android.f06b9089.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4277dd76d55b81fb853c",
+    "url": "/static/js/main.4277dd76.chunk.js"
+  },
+  {
+    "revision": "aec89eacd058bf00e130",
+    "url": "/static/js/1.aec89eac.chunk.js"
+  },
+  {
+    "revision": "4277dd76d55b81fb853c",
+    "url": "/static/css/main.cfd85197.chunk.css"
+  },
+  {
+    "revision": "ed37c16999d3e4c48eb2d6caf967b0f1",
+    "url": "/index.html"
+  }
+];
