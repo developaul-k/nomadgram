098ff4f65b2a6e36a3fc2400cc72c9d69fef7c04
김영주 <pual@gim-yeongjuui-MacBook-Pro.local>
2019-03-21 13:24:47 +0900

    bulls

--- nomadgram/views.py
+++ nomadgram/views.py
@@ -2,25 +2,6 @@ from django.views.generic import View
 from django.http import HttpResponse
 from django.conf import settings
 import os
-import boto3
-from boto3.s3.transfer import S3Transfer
-
-local_directory = './media'
-transfer = S3Transfer(boto3.client('s3', 'ap-northeast-2',
-                                   aws_access_key_id = 'AKIAJAI34VUCXMRZUA3A',
-                                   aws_secret_access_key='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'))
-client = boto3.client('s3')
-bucket = 'youngram'
-for root, dirs, files in os.walk(local_directory):
-    for filename in files:
-        local_path = os.path.join(root, filename)
-        relative_path = os.path.relpath(local_path, local_directory)
-        s3_path = os.path.join('your s3 path',relative_path)
-        if filename.endswith('.pdf'):
-            transfer.upload_file(local_path, bucket, s3_path,extra_args={'ACL': 'public-read'})
-        else:
-            transfer.upload_file(local_path, bucket, s3_path)
-
 
 class ReactAppView(View):
 
